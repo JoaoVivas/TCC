7d5b8aaa5e113dc6d0b4c76ca0f7ffa14b33819b
JoaoVivas <78926533+JoaoVivas@users.noreply.github.com>
2023-07-05 23:34:43 -0300

    Chore/metodologia tests (#10)
    * feat: add jerk ceq code
    
    * chore: random

--- TCC/MatLab/a_main2.m
+++ TCC/MatLab/a_main2.m
@@ -8,14 +8,16 @@ base_command
 
 initial_guess = false;
 %%
-runge_kutta_preview
+% runge_kutta_preview
 %%
-% hargraves_base_x2
+tic
+hargraves_base_x2
+toc
 % runge_kutta_base_x2
 % runge_kutta_x2
 % runge_kutta_x4
- hargraves_x2
-% hargraves_x4 % roda mais ou menos mas foge da curva mesmo com a função de otimização
+% hargraves_x2
+% hargraves_x4 % roda mais ou menos mas foge da curva mesmo com a funï¿½ï¿½o de otimizaï¿½ï¿½o
 % hargraves_x3 % horrivel
 % hargraves_x5 % horrrivel
 
@@ -35,16 +37,19 @@ plot(r_t,r_vel_x,r_t,r_vel_y,r_t,r_vel_xb,r_t,r_vel_yb)
 title ('vx_vy(t) ponta e base otimizada')
 legend('vx da ponta',' vy da ponta ','vx da base ', 'vy da base')
 %% --------------------------------------- Runge Kutta Result --------------------------------
-r_s0 = [0 0 0 0];
-r_base(1,:) = r_des_xb;
-r_base(2,:) = r_des_yb;
-r_base(3,:) = r_vel_xb;
-r_base(4,:) = r_vel_yb;
+% r_s0 = [0 0 0 0];
+% r_base(1,:) = r_des_xb;
+% r_base(2,:) = r_des_yb;
+% r_base(3,:) = r_vel_xb;
+% r_base(4,:) = r_vel_yb;
 
-[r_s_base] = runge_kutta(r_s0, r_base, t_base, @dynamic_model);
+% [r_s_base] = runge_kutta(r_s0, r_base, t_base, @dynamic_model);
 
-figure(1001)
-plot(r_s_base(1,:),r_s_base(2,:),r_base(1,:),r_base(2,:))
-title ('x_y da ponta otimiada e base otimizada')
-legend('ponta','base')
+% figure(1001)
+% plot(r_s_base(1,:),r_s_base(2,:),r_base(1,:),r_base(2,:))
+% title ('x_y da ponta otimiada e base otimizada')
+% legend('ponta','base')
+
+max_ceq_defect = max(ceq_har)
+max_ceq_jerk = max(ceq_jerk)
 
