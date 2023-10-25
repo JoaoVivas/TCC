925bde59cf710c9d7c75be8507b53c5995564a2b
JoaoVivas <78926533+JoaoVivas@users.noreply.github.com>
2023-10-24 23:20:13 -0300

    Revisions1.2 (#16)
    * remove duplicate citation and version conflict
    
    * add gcode image and Wander Revisions
    
    * Salvando seu popo
    
    * Update resultados_e_discussao.tex
    
    * rearange topics and sections
    
    * add revisions pdf
    
    * ignore json files
    
    * ortografi and clean up
    
    * remove text and fix spell
    
    * remove and fix spell
    
    * update matlab fig script
    
    * update git ignore and fix fig name
    
    * change file name
    
    * comments revision
    
    * revisions wander
    
    * fix figures placement and Resumo/Introdução/Manufatura aditiva text
    
    * text resumo abstract introduction referencial teorico
    
    * change equation
    
    * add release 1.2
    
    ---------
    
    Co-authored-by: nannester <147570756+nannester@users.noreply.github.com>

--- TCC/MatLab/script.m
+++ TCC/MatLab/script.m
@@ -4,36 +4,53 @@ path = 'C:\Users\Augusto\TCC\TCC\MatLab\results\';
 dir_array = dir (fullfile(path,'*'));
 numel(dir_array)
 %%
-for ii = 4:4 %numel(dir_array) % loop over subfolders.
+for ii = 3:4 %numel(dir_array) % loop over subfolders.
     file_array = dir(fullfile(path,dir_array(ii).name,'*.fig'));
     
     for i = 1:numel(file_array)
        file_name = file_array(i).name;
        file_path = (fullfile(path,dir_array(ii).name, file_name));
        
-       fig = openfig(file_path)
+       fig = openfig(file_path);
        
-       ttl = get(get(gca, 'title'), 'string')
+%        set(gca, 'OuterPosition', [200 200 100 100])
+       title(gca, '');
        
-       ttl = strrep(ttl, 'Resultado x_y(t)', '');
-       ttl = strrep(ttl, 'Resultado vx_vy(t)', '');
-       ttl = strrep(ttl, 'Resultado x_y da', '');
-       ttl = strrep(ttl, 'Viabilidade x num_f Teste', '');
+       switch false
+            case isempty(strfind(file_name, 'des'))
+                y_axis = 'desocamento [mm]';
+                x_axis = 'tempo [s]';
+            case isempty(strfind(file_name, 'pos'))
+                fig.OuterPosition = [0 0 598 1200];
+                y_axis = 'posição x [mm]';
+                x_axis = 'posição y [mm]';
+                xlim([-1 11]);
+                ylim([-1 11]);
+                ax = gca;
+                ax.XTick = [0 2 4 6 8 10];
+                ax.YTick = [0 2 4 6 8 10];
+           case isempty(strfind(file_name, 'vels'))
+                y_axis = 'velocidade [mm/s]';
+                x_axis = 'tempo [s]';
+           case isempty(strfind(file_name, 'Viabilidade'))
+                y_axis = 'viabilidade []';
+                x_axis = 'numero de iterações []';
+       end
        
-       ttl = strrep(ttl, '1', '');
-       ttl = strrep(ttl, '2', '');
-       ttl = strrep(ttl, '3', '');
-       ttl = strrep(ttl, '4', '');
-       ttl = strrep(ttl, '5', '');
-       
-       case_str = strrep(file_name, ' ', '')
-       case_str = strrep(case_str, 'des.png', '')
-       case_str = strrep(case_str, 'pos.png', '')
-       case_str = strrep(case_str, 'vels.png', '')
-       case_str = strrep(case_str, 'Viabilidade.png', '')
-       case_str = strrep(case_str, 'Teste', '(Caso ')
-       
-       ttl = strcat(ttl, case_str, ')')
+       xlabel(gca, x_axis)
+       ylabel(gca, y_axis)
+%        
+%        x_axis = ''
+%        y_axis = ''
+%        
+%        case_str = strrep(file_name, ' ', '')
+%        case_str = strrep(case_str, 'des.png', '')
+%        case_str = strrep(case_str, 'pos.png', '')
+%        case_str = strrep(case_str, 'vels.png', '')
+%        case_str = strrep(case_str, 'Viabilidade.png', '')
+%        case_str = strrep(case_str, 'Teste', '(Caso ')
+%        
+%        ttl = strcat(ttl, case_str, ')')
        
        set(gca,'FontSize',20)
        set(findall(gca, 'Type', 'Line'),'LineWidth',3);
@@ -45,7 +62,7 @@ for ii = 4:4 %numel(dir_array) % loop over subfolders.
        lgd = legend('Location', 'southoutside', 'Orientation', 'horizontal');
        lgd.FontSize = 16;
        new_file_path = strrep(file_path, '.fig', '.png');
-       saveas(gcf, new_file_path)
+       % saveas(gcf, new_file_path)
     end
 end
 
